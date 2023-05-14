06b20919d21cf2c8d03ec403232e53c166569905
Catalin Iosif <catalin.iosif@nshift.com>
2023-05-14 13:37:10 +0300

    implement args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use args::Args;
+use clap::Parser;
+use crate::args::Commands::{Decode, Encode, Print, Remove};
+
 mod args;
 mod chunk;
 mod chunk_type;
@@ -9,6 +13,16 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
     //todo!()
-    {}
+    {
+        let args = Args::parse();
+        match &args.command {
+            Encode {path, chunk_type, message} => {
+                println!("Called encode with path: {}, chunk_t: {}, and message: {}", path, chunk_type, message);
+            },
+            Decode {path, chunk_type} => (),
+            Remove {path, chunk_type} => (),
+            Print {path} => ()
+        }
+    }
     Ok(())
 }
